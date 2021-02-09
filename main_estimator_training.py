57888db5337b6bcc5d402722c49b962aa9b323eb
Yannic <y.lieder@gmx.de>
2021-02-09 21:37:07 +0100

    Update CLI tool for estimator training

--- main_estimator_training.py
+++ main_estimator_training.py
@@ -9,43 +9,44 @@ from sklearn.metrics import classification_report
 from GenderEstimator import GenderEstimator
 
 
-def main():
-    data_dir = Path('data/datasets')
-    estimator_dir = Path('data/estimators')
+def train_gender_estimator(n_reviews, max_features):
+    """
+    Trains a gender estimator with C=1.
+    
+    :param n_reviews: Specifies the dataset to be used for training (number of reviews per user).
+    :param max_features: Maximum number of features.
+    """
+    data_dir = Path('data')
+    dataset_path = data_dir / f'dataset_{n_reviews}_train.pkl'
+    estimator_dir = data_dir / 'estimators'
     estimator_dir.mkdir(exist_ok=True)
 
-    if len(sys.argv) != 2:
-        print("Specify number of reviews per sample (1, 2, 5, 10, 20 or all)")
-
-    n_reviews = sys.argv[1]
-    print("n_reviews: ", n_reviews)
-
-    with open(data_dir / f'dataset_{n_reviews}_train.pkl', 'rb') as fd:
+    with open(dataset_path, 'rb') as fd:
         data = pickle.load(fd)
-        X_train = [''.join(reviews) for gender, reviews in data]
+        X_train = [' '.join(reviews) for gender, reviews in data]
         y_train = [gender for gender, reviews in data]
 
-    with open(data_dir / f'dataset_{n_reviews}_test.pkl', 'rb') as fd:
-        data = pickle.load(fd)
-        X_test = [''.join(reviews) for gender, reviews in data]
-        y_test = [gender for gender, reviews in data]
-
-    estimator = GenderEstimator(max_features=10_000, C=1.0)
+    estimator = GenderEstimator(max_features=max_features, C=1.0)
 
     start = timer()
     estimator.fit(X_train, y_train)
     end = timer()
     print(f"Fit estimator in {timedelta(seconds=end-start)}")
 
-    with open(estimator_dir / f'estimator_{n_reviews}.pkl', 'wb') as fd:
+    output_path = estimator_dir / f'estimator_{n_reviews}.pkl'
+    with open(output_path, 'wb') as fd:
         pickle.dump(estimator, fd)
-
-    # training_score = estimator.score(X_train, y_train)
-    # print(f"Training accuracy: {training_score}")
-
-    y_pred = estimator.predict(X_test)
-    print(classification_report(y_test, y_pred))
+        print(f"Export trained estimate to {output_path}")
 
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) < 2:
+        print("Specify number of reviews per user")
+        
+    if len(sys.argv) == 3:
+        max_features = int(sys.argv[2])
+    else:
+        max_features = 10_000
+    
+    dataset_path = sys.argv[1]
+    train_gender_estimator(dataset_path, max_features)
