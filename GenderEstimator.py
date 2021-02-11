322a57f8d81a305472b3002f3374d8038176799f
Yannic <y.lieder@gmx.de>
2021-02-11 09:29:24 +0100

    Add comments to GenderEstimator.py

--- GenderEstimator.py
+++ GenderEstimator.py
@@ -6,7 +6,15 @@ from sklearn.svm import SVC
 
 
 class GenderEstimator(BaseEstimator):
+    """
+    Estimator to predict gender of a Yelp user given her written reviews.
+    """
     def __init__(self, max_features: int = 1000, C: float = 1.0, gamma: Union[str, float] = 'scale'):
+        """
+        :param max_features: Maximum number of features, extracted from the train dataset.
+        :param C: Regularization parameter of the SVM.
+        :param gamma: Kernel coefficient for RBF kernel of the SVM.
+        """
         self.max_features = max_features
         self.gamma = gamma
         self.C = C
@@ -14,6 +22,10 @@ class GenderEstimator(BaseEstimator):
         self.vectorizer = None
 
     def fit(self, X, y):
+        """
+        :param X: Training data (reviews). One review per sample. Multiple reviews should be merged before.
+        :param y: Training labels (gender). Either Gender.F or Gender.M.
+        """
         self.vectorizer = TfidfVectorizer(max_features=self.max_features)
         self.clf = SVC(C=self.C, gamma=self.gamma)
 
@@ -21,9 +33,19 @@ class GenderEstimator(BaseEstimator):
         self.clf.fit(X_vectorized, y)
 
     def predict(self, X):
+        """
+        :param X: List of reviews, one review per sample. Multiple reviews should be merged before.
+        :return: Predicted gender of each sample.
+        """
         X_vectorized = self.vectorizer.transform(X)
         return self.clf.predict(X_vectorized)
 
     def score(self, X, y):
+        """
+        Same as predict, but computes accuracy between prediction and y.
+
+        :param X: List of reviews, one review per sample. Multiple reviews should be merged before.
+        :param y: True gender.
+        """
         X_vectorized = self.vectorizer.transform(X)
         return self.clf.score(X_vectorized, y)
--- main_parameter_optimization.py
+++ main_parameter_optimization.py
@@ -8,12 +8,18 @@ from GenderEstimator import GenderEstimator
 
 
 def main():
+    """
+    Runs an extensive grid search for n_reviews = 1, 2, 5, 10, 'all', Hyperparameters C = 0.1, 1, 10, 100, 1000
+    and max_features = 500, 1000, 5000, 10000.
+
+    Outputs the best parameters for each n in n_reviews.
+    """
     for n_reviews in (1, 2, 5, 10, 'all'):
         print("n_reviews: ", n_reviews)
 
         n_samples = 20_000
 
-        dataset_path = 'data/datasets/dataset_{n_reviews}_train.pkl'
+        dataset_path = f'data/datasets/dataset_{n_reviews}_train.pkl'
         with open(dataset_path, 'rb') as fd:
             data = pickle.load(fd)
             X_train = [' '.join(reviews) for gender, reviews in data][:n_samples]
@@ -33,7 +39,7 @@ def main():
         ps = PredefinedSplit(test_fold=np.concatenate(
             (-np.ones(int(train_size * len(X_train))), np.zeros(ceil(validation_size * len(X_train)))))
         )
-        gs = GridSearchCV(GenderEstimator(), param_grid, cv=ps, verbose=3)
+        gs = GridSearchCV(GenderEstimator(), param_grid, cv=ps, n_jobs=4, verbose=3)
 
         gs.fit(corpus_train, y_train)
 
